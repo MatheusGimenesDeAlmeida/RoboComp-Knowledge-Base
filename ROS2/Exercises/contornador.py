77e6fa425baac7009f3b9517bfb40778bf58c483
Matheus Almeida <matheusga2@al.insper.edu.br>
2024-03-31 17:28:02 -0300

    Adição de exercício ROS2

---
+++ ROS2/Exercises/contornador.py
@@ -0,0 +1,12 @@
+# O objetivo desse robô é contornar as 3 paredes e depois dar uma volta completa em volta delas 
+
+
+# Primeiro chega à 0.5 metros da parede, gira no sentido anti-horário, e começa a seguir reto
+
+# Quando identifica que seu sensor do lado direito está com muito espaço, ele faz uma curva de 180 grais no sentido horário
+
+# Quando acaba a curva continua reto, encontra um vazio do seu lado ESQUERDO, e faz uma curva 180 graus anti-horária e segue reto 
+
+# Quando identifica vazio do seu lado DIREITO, faz uma curva 180 graus sentido HORÀRIO, segue reto até achar um vario do seu lado esquerdo
+
+# Dpois contar fazendo uma curva com um raio muito maior
